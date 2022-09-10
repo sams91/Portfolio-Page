154e81dcb97e6892e57873e58636a964d7003add
sams91 <103255663+sams91@users.noreply.github.com>
2022-09-09 19:40:50 -0500

    Added a contact page and tweaked a few things.

---
+++ js/loan.js
@@ -0,0 +1,90 @@
+const loanAmountInput = document.querySelector(".loan-amount");
+const interestRateInput = document.querySelector(".interest-rate");
+const loanTenureInput = document.querySelector(".loan-tenure");
+
+const loanEMIValue = document.querySelector(".loan-emi .value");
+const totalInterestValue = document.querySelector(".total-interest .value");
+const totalAmountValue = document.querySelector(".total-amount .value");
+
+const calculateBtn = document.querySelector(".calculate-btn");
+
+let loanAmount = parseFloat(loanAmountInput.value);
+let interestRate = parseFloat(interestRateInput.value);
+let loanTenure = parseFloat(loanTenureInput.value);
+
+let interest = interestRate / 12 / 100;
+
+let myChart;
+
+const displayChart = (totalInterestValue, loanAmount) => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    myChart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+        labels: ['Total Interest', 'Principal Loan Amount'],
+        datasets: [{           
+            data: [totalInterestValue, loanAmount],
+            backgroundColor: [
+                "#e63946", "#14213d" 
+            ],
+            borderWidth: 0
+        }]
+    },
+});
+}
+
+const updateChart = (totalInterestValue, loanAmount) => {
+    myChart.data.datasets[0].data[0] = totalInterestValue;
+    myChart.data.datasets[0].data[1] = loanAmount;
+    myChart.update();
+}
+
+const calculateEMI = () => {
+    let emi = 
+    loanAmount * 
+    interest * 
+    (Math.pow(1 + interest, loanTenure) / 
+    (Math.pow(1 + interest, loanTenure) - 1));
+
+
+    return emi;
+
+};
+
+
+
+const updateData = (emi) => {
+    loanEMIValue.innerHTML = Math.round(emi);
+
+    let totalAmount = Math.round(loanTenure * emi);
+    totalAmountValue.innerHTML = totalAmount;
+
+    let totalInterest = Math.round(totalAmount - loanAmount);
+    totalInterestValue.innerHTML = totalInterest;
+
+    if (myChart) {
+        updateChart(totalInterest, loanAmount);
+    } else {
+        displayChart(totalInterest, loanAmount);
+    }
+    
+};
+
+const refreshInputValues = () => {
+    loanAmount = parseFloat(loanAmountInput.value);
+    interestRate = parseFloat(interestRateInput.value);
+    loanTenure = parseFloat(loanTenureInput.value);
+    interest = interestRate / 12 / 100;
+}
+
+const init = () => {
+    refreshInputValues();
+    let emi = calculateEMI();
+    updateData(emi);
+}
+
+init();
+
+
+
+calculateBtn.addEventListener("click", init);
